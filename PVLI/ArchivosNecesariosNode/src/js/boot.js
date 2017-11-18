4a175eb95499dea61a771f71265eaf29c2800ae3
Diego Baratto <dbaratto@ucm.es>
2017-11-18 14:25:44 +0100

    TPVThings

--- PVLI/ArchivosNecesariosNode/src/js/boot.js
+++
@@ -1,9 +0,0 @@
-var bootState = {
-	create: function(){
-		//en este estado damos inicio a las fisicas de tipo ARCADE yey
-		game.physics.startSystem(Phaser.Physics.ARCADE);
-
-		//llamamos al siguiente estado -> cargar recursos
-		game.state.start('load');
-	}
-};
