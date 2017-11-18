40c24a8f664134020c19be569215b218833891be
Diego Baratto <dbaratto@ucm.es>
2017-11-18 14:19:37 +0100

    eda things

--- PVLI/EjerciciosPrueba/2ª/main.js
+++
@@ -1,9 +0,0 @@
-window.onload = function(){
-	var button = document.getElementById('summon');
-	button.addEventListener('click', function(){
-		//console.log('Summon!');
-		var img = new Image();
-		img.src = 'https://placekitten.com/g/200/200/';
-		document.getElementById('kittens').appendChild(img);
-	});
-};
